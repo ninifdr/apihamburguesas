366fb91e6ded5fb221169f9f6705a2ed076931d8
Bernardita <bfernandezdelrio1@uc.cl>
2020-05-04 23:42:14 -0400

    cambio chico

--- app.py
+++ app.py
@@ -222,7 +222,7 @@ def delete_ingrediente_id(id):
     if(len(all_ingredientes_hamburguesas) > 0):
         response = {'message': 'Ingrediente no se puede borrar, se encuentra presente en una hamburguesa'}
         return jsonify(response), 409
-    elif(len(all_ingredientes) != 0 and len(all_ingredientes_hamburguesas) == 0:
+    elif(len(all_ingredientes) != 0 and len(all_ingredientes_hamburguesas) == 0):
         ingredientes.delete_many({"id": id})
         response = {'message': 'Ingrediente eliminado'}
         return jsonify(response), 200
